635ae02a0f16826604a3b7008ab6d754109c2659
Rob Crocombe <rjwc1993@yahoo.co.uk>
2016-12-29 19:11:24 +0000

    Refactored code for ES2015, added eslint

--- post.js
+++ post.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-"use strict";
 
-const DRAFT_DIR = process.cwd() + '/_drafts';
-const POST_DIR = process.cwd() + '/_posts';
-var program = require('commander');
-var rl = require('readline-sync');
-var matter = require('gray-matter');
-var moment = require('moment');
-var fs = require('fs-extra');
+const program = require('commander');
+const rl = require('readline-sync');
+const matter = require('gray-matter');
+const moment = require('moment');
+const fs = require('fs-extra');
+const dir = require('path');
+
+const DRAFT_DIR = dir.join(process.cwd(), '_drafts');
+const POST_DIR = dir.join(process.cwd(), '_posts');
 
 program
   .version('1.0.0')
@@ -21,19 +22,17 @@ checkJekyllExists();
 
 if (program.draft) {
   createDraft(program.draft);
-}
-else if (program.publish) {
+} else if (program.publish) {
   publishPost(program.publish);
-}
-else if (program.unpublish) {
+} else if (program.unpublish) {
   unpublishPost(program.unpublish);
 }
 
 function createDraft(postName) {
-  var fileName = parseName(postName);
-  var target = `${DRAFT_DIR}/${fileName}.md`;
+  const fileName = parseName(postName);
+  const target = dir.join(DRAFT_DIR, `${fileName}.md`);
 
-  var frontMatter = {
+  const frontMatter = {
     layout: 'post',
     title: postName,
     slug: fileName
@@ -42,21 +41,18 @@ function createDraft(postName) {
   if (fileExists(target)) {
     if (rl.keyInYN(`Draft '${target}' already exists. Overwrite?`)) {
       writeFileThenExit(target, fileName, frontMatter);
-    }
-    else {
+    } else {
       exit(0);
     }
-  }
-  else {
+  } else {
     writeFileThenExit(target, fileName, frontMatter);
   }
 }
 
 function publishPost(postName) {
-  if (postName == true) {
+  if (postName === true) {
     publishAllPosts();
-  }
-  else {
+  } else {
     publishSinglePost(`${parseName(postName)}.md`, true);
   }
 }
@@ -66,9 +62,8 @@ function publishAllPosts() {
     if (err) {
       console.log(err);
       exit(1);
-    }
-    else {
-      for (var file of files) {
+    } else {
+      for (const file of files) {
         publishSinglePost(file, false);
       }
       exit(0);
@@ -77,69 +72,60 @@ function publishAllPosts() {
 }
 
 function publishSinglePost(fileName, exitOnWrite) {
-  var source = `${DRAFT_DIR}/${fileName}`;
+  const source = dir.join(DRAFT_DIR, fileName);
 
   if (fileExists(source)) {
-    var post = matter.read(source);
-    var now = moment();
+    const post = matter.read(source);
+    const now = moment();
     post.data.date = getDateTime(now);
 
-    var publishName = `${getDateOnly(now)}-${fileName}`;
-    var target = `${POST_DIR}/${publishName}`;
+    const publishName = `${getDateOnly(now)}-${fileName}`;
+    const target = dir.join(POST_DIR, publishName);
 
     if (fileExists(target)) {
       if (rl.keyInYN(`Post '${target}' already exists. Overwrite?`)) {
         deleteFile(source);
         if (exitOnWrite) {
           writeFileThenExit(target, publishName, post.data, post.content);
-        }
-        else {
+        } else {
           writeFile(target, publishName, post.data, post.content);
         }
-      }
-      else if (exitOnWrite) {
+      } else if (exitOnWrite) {
         exit(0);
       }
-    }
-    else if (exitOnWrite) {
+    } else if (exitOnWrite) {
       deleteFile(source);
       writeFileThenExit(target, publishName, post.data, post.content);
-    }
-    else {
+    } else {
       deleteFile(source);
       writeFile(target, publishName, post.data, post.content);
     }
-  }
-  else {
+  } else {
     console.log(`Draft '${source}' not found.`);
     exit(1);
   }
 }
 
 function unpublishPost(postName) {
-  var fileName = `${parseName(postName)}.md`;
+  const fileName = `${parseName(postName)}.md`;
 
   fs.readdir(POST_DIR, (err, files) => {
     if (err) {
       console.log(err);
       exit(1);
-    }
-    else {
-      for (var file of files) {
+    } else {
+      for (const file of files) {
         if (file.substring(11) === fileName) {
-          var source = `${POST_DIR}/${file}`
-          var target = `${DRAFT_DIR}/${fileName}`;
+          const source = dir.join(POST_DIR, file);
+          const target = dir.join(DRAFT_DIR, fileName);
 
           if (fileExists(target)) {
             if (rl.keyInYN(`Draft '${target}' already exists. Overwrite?`)) {
               moveFileThenExit(source, target, fileName);
               return;
             }
-            else {
-              exit(0);
-            }
-          }
-          else {
+            exit(0);
+          } else {
             moveFileThenExit(source, target, fileName);
             return;
           }
@@ -154,7 +140,7 @@ function unpublishPost(postName) {
 function parseName(val) {
   return val
     .toLowerCase()
-    .replace(/['".,\/#!?$%\^&\*;:{}=\`~()]/g, '')
+    .replace(/['".,/#!?$%^&*;:{}=`~()]/g, '')
     .replace(/[^A-Za-z0-9_-]+/g, '-');
 }
 
@@ -167,19 +153,17 @@ function getDateOnly(date) {
 }
 
 function checkJekyllExists() {
-  var draftDirExist = fileExists(DRAFT_DIR);
-  var postDirExist = fileExists(POST_DIR);
+  const draftDirExist = fileExists(DRAFT_DIR);
+  const postDirExist = fileExists(POST_DIR);
 
   if (!draftDirExist && !postDirExist) {
-    console.log(`Draft and post folders not found. Are you in a Jekyll project?`);
+    console.log('Draft and post folders not found. Are you in a Jekyll project?');
     exit(1);
-  }
-  else if (!draftDirExist) {
-    console.log(`Draft folder not found. Are you in a Jekyll project?`);
+  } else if (!draftDirExist) {
+    console.log('Draft folder not found. Are you in a Jekyll project?');
     exit(1);
-  }
-  else if (!postDirExist) {
-    console.log(`Post folder not found. Are you in a Jekyll project?`);
+  } else if (!postDirExist) {
+    console.log('Post folder not found. Are you in a Jekyll project?');
     exit(1);
   }
 }
@@ -188,8 +172,7 @@ function fileExists(path) {
   try {
     fs.accessSync(path);
     return true;
-  }
-  catch(err) {
+  } catch (err) {
     return false;
   }
 }
@@ -208,29 +191,24 @@ function moveFileThenExit(source, target, fileName) {
 function deleteFile(path) {
   try {
     fs.removeSync(path);
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
     exit(1);
   }
 }
 
-function writeFile(path, name, frontMatter, content) {
-  content = content || '';
-
+function writeFile(path, name, frontMatter, content = '') {
   try {
     fs.writeFileSync(path, matter.stringify(content, frontMatter));
-    var type = program.draft ? 'Draft' : 'Post';
+    const type = program.draft ? 'Draft' : 'Post';
 
     if (name.endsWith('.md')) {
       console.log(`${type} '${name}' created.`);
-    }
-    else {
+    } else {
       console.log(`${type} '${name}.md' created.`);
     }
     return true;
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
     return false;
   }
@@ -239,8 +217,7 @@ function writeFile(path, name, frontMatter, content) {
 function writeFileThenExit(path, name, frontMatter, content) {
   if (writeFile(path, name, frontMatter, content)) {
     exit(0);
-  }
-  else {
+  } else {
     exit(1);
   }
 }
